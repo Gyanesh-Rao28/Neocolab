d0a1c4353198cb8053fa22fc8f39114b630de2bc
Gyanesh Rao <gyaneshrao28@gmail.com>
2023-02-14 23:08:29 +0530

    init

---
+++ week1/Q3.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+// import javax.sound.sampled.SourceDataLine;
+
+import java.lang.Math;
+
+class Q3 {
+
+    public static void main(String[] args){
+        double f,c;
+
+        Scanner s = new Scanner(System.in);
+
+        c = s.nextInt();
+
+        f=1.8*c+32;
+        System.out.println(f);
+}
+    }
+
