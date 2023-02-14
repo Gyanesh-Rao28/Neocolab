14c5fd33fde8eeffee450cda242251b91c127182
Gyanesh Rao <gyaneshrao28@gmail.com>
2023-02-14 21:48:24 +0530

    init

---
+++ Q1.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+class Q1 {
+    public static void main(String args[]) {
+        int x, y, l;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("val");
+
+        x = s.nextInt();
+        y = s.nextInt();
+        l = s.nextInt();
+
+        x = x + (l / 2);
+        y = y + (l / 2);
+
+        System.out.println(x + " " + y);
+
+        s.close();
+    }
+}
